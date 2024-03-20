5936b3f2d09109af2d1365bac1058b3988e0d275
chakri pavan kumar <siriyalapavankumar@gmail.com>
2024-03-20 20:47:19 +0530

    yet to start

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       if (details.url.includes("instagram.com")) {
-        return { redirectUrl: "https://yourwebsite.com" };
+        return { redirectUrl: "https://app.100xdevs.com/" };
       }
     },
     { urls: ["*://*.instagram.com/*"] },
